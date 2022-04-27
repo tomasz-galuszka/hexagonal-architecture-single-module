06eacb80d6c6b20c92e910f16c2dcdf99722e686
atomaszg <tomasz.galuszka@allegro.pl>
2022-04-27 18:00:14 +0200

    extracted domain apis

--- src/main/kotlin/com/galuszkat/hexagonal/architecture/baskets/domain/port/BasketMessageBrokerPort.kt
+++ src/main/kotlin/com/galuszkat/hexagonal/architecture/baskets/domain/port/BasketMessageBrokerPort.kt
@@ -3,9 +3,7 @@ package com.galuszkat.hexagonal.architecture.baskets.domain.port
 import com.galuszkat.hexagonal.architecture.baskets.domain.model.Basket
 
 interface BasketMessageBrokerPort {
-
   fun publishCreated(basket: Basket)
-
   fun publishUpdated(basket: Basket)
   fun publishDeleted(basket: Basket)
 }
--- src/main/kotlin/com/galuszkat/hexagonal/architecture/baskets/domain/port/BasketRepositoryPort.kt
+++ src/main/kotlin/com/galuszkat/hexagonal/architecture/baskets/domain/port/BasketRepositoryPort.kt
@@ -3,11 +3,9 @@ package com.galuszkat.hexagonal.architecture.baskets.domain.port
 import com.galuszkat.hexagonal.architecture.baskets.domain.model.Basket
 
 interface BasketRepositoryPort {
-
   fun store(basket: Basket): Basket
-
   fun findById(id: String): Basket?
-
   fun update(basket: Basket)
   fun delete(basket: Basket)
 }
+s
